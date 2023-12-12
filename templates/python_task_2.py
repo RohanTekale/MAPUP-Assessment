93a2cdd45cba78a09f705a72505e6e4431288b27
Rohan Rajkumar Tekale <116381258+Rorate7@users.noreply.github.com>
2023-12-12 13:12:15 +0530

    Add files via upload

---
+++ templates/python_task_2.py
@@ -0,0 +1,78 @@
+import pandas as pd
+
+
+def calculate_distance_matrix(df)->pd.DataFrame():
+    """
+    Calculate a distance matrix based on the dataframe, df.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        pandas.DataFrame: Distance matrix
+    """
+    # Write your logic here
+
+    return df
+
+
+def unroll_distance_matrix(df)->pd.DataFrame():
+    """
+    Unroll a distance matrix to a DataFrame in the style of the initial dataset.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
+    """
+    # Write your logic here
+
+    return df
+
+
+def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
+    """
+    Find all IDs whose average distance lies within 10% of the average distance of the reference ID.
+
+    Args:
+        df (pandas.DataFrame)
+        reference_id (int)
+
+    Returns:
+        pandas.DataFrame: DataFrame with IDs whose average distance is within the specified percentage threshold
+                          of the reference ID's average distance.
+    """
+    # Write your logic here
+
+    return df
+
+
+def calculate_toll_rate(df)->pd.DataFrame():
+    """
+    Calculate toll rates for each vehicle type based on the unrolled DataFrame.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        pandas.DataFrame
+    """
+    # Wrie your logic here
+
+    return df
+
+
+def calculate_time_based_toll_rates(df)->pd.DataFrame():
+    """
+    Calculate time-based toll rates for different time intervals within a day.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        pandas.DataFrame
+    """
+    # Write your logic here
+
+    return df
